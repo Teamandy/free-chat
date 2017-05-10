67227a4c0c4d50ae262d0dec9c7789430f9d795a
Teamandy <andytimirov@yandex.ru>
2017-05-10 09:04:37 +0300

    fixed image path to lowercase

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -7,7 +7,7 @@ class Friend extends React.Component {
         return (
             <Link to={`/chat/${name.toLowerCase()}`}>
                 <li ref={(li)=>{this.li = li}} className={activeName===true ? "friend__item active" : "friend__item"}>
-                    <img src={`/${name}.png`} alt="icon" />
+                    <img src={`/${name.toLowerCase()}.png`} alt="icon" />
                     <span className="friend__name">{name}</span>
                 </li>
             </Link>
